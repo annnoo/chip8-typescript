1850531e945a182f69e2821f54024ae651635090
Anno <annnoocodes@gmail.com>
2020-07-20 23:31:02 +0200

    added random generation (todo: fix blur)

--- src/CanvasRenderer.ts
+++ src/CanvasRenderer.ts
@@ -16,14 +16,16 @@ export class CanvasRenderer extends Renderer {
 
   render(pixels: boolean[]): void {
     this.context.putImageData(this.createImageData(), 0, 0)
+
+
+    this.context.drawImage(this.canvas, 0, 0, this.resolution.width, this.resolution.height, 0, 0, this.canvas.width, this.canvas.height)
+
   }
 
   createImageData() {
-    const imageData: ImageData = new ImageData(this.resolution.width * this.scale, this.resolution.height * this.scale);
+    const imageData: ImageData = new ImageData(this.resolution.width, this.resolution.height);
     for (let i = 0; i < imageData.data.length; i += 4) {
-
-      imageData.data.set(WHITE, i);
-
+      Math.floor(Math.random() * 2) < 1 ? imageData.data.set(BLACK, i) : imageData.data.set(RED, i);
     }
     return imageData;
   }
